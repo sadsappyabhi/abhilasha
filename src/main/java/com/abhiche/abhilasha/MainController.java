e0d5bf7254f9744bf993f7e9ce134cb1c100137f
Abhilasha <sadsappyabhi@proton.me>
2026-02-19 18:11:48 -0600

    initial commit w/ default .gitignore

---
+++ src/main/java/com/abhiche/abhilasha/MainController.java
@@ -0,0 +1,54 @@
+package com.abhiche.abhilasha;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @RequestMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+
+    @RequestMapping("/bio")
+    public String bio(Model model) {
+        model.addAttribute("pageTitle", "Bio");
+        return "bio";
+    }
+
+    @RequestMapping("/nullcopula")
+    public String nullCopula(Model model) {
+        model.addAttribute("pageTitle", "Null Copula");
+        return "null-copula";
+    }
+
+    @RequestMapping("/turtleboat")
+    public String turtleBoat(Model model) {
+        model.addAttribute("pageTitle", "Turtle Boat");
+        return "turtle-boat";
+    }
+
+    @RequestMapping("/recordings")
+    public String recordings(Model model) {
+        model.addAttribute("pageTitle", "Recordings");
+        return "recordings";
+    }
+
+    @RequestMapping("/performances")
+    public String performances(Model model) {
+        model.addAttribute("pageTitle", "Performances");
+        return "performances";
+    }
+
+    @RequestMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("pageTitle", "Contact / Links");
+        return "contact";
+    }
+}
