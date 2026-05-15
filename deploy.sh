e3391dfc30297a623b8ffc8ab7d435036f920cea
Abby <abby@mocalabs.dev>
2026-05-14 23:33:54 -0500

    Updated deploy script to use self-hosted registry instead of docker hub. Tested pulling & deploying images on mocalabs VPS. Success!

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Config
+REGISTRY="registry.mocalabs.dev"
 APP_REPO="mocalabs/abhilasha"
 DB_REPO="mocalabs/abhilasha-db"
 TAG="latest"
 
 echo "Building Application Image ..."
-docker build --no-cache -t ${APP_REPO}:${TAG} -f Dockerfile .
+docker build --no-cache -t ${REGISTRY}/${APP_REPO}:${TAG} -f Dockerfile .
 
 echo "Building Database Image ..."
-docker build --no-cache -t ${DB_REPO}:${TAG} -f Dockerfile-db .
+docker build --no-cache -t ${REGISTRY}/${DB_REPO}:${TAG} -f Dockerfile-db .
 
 echo "Pushing Application Image to Registry ..."
-docker push ${APP_REPO}:${TAG}
+docker push ${REGISTRY}/${APP_REPO}:${TAG}
 
 echo "Pushing Database Image to Registry ..."
-docker push ${DB_REPO}:${TAG}
+docker push ${REGISTRY}/${DB_REPO}:${TAG}
