4117db9fa00d1286ab23da36791e8d48c84133e9
Abby <abby@mocalabs.dev>
2026-05-17 00:17:36 -0500

    Updated deploy script to prompt user and add error checking. Removed build/push of db container

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,29 @@ APP_REPO="mocalabs/abhilasha"
 DB_REPO="mocalabs/abhilasha-db"
 TAG="latest"
 
+echo "WARNING: You must be logged in to the registry via 'docker login' for this script to run!"
+
+read -p "Have you logged in with 'docker login'? (y/n) " IS_LOGGED_IN
+
+if [[ "$IS_LOGGED_IN" == "y" || "$IS_LOGGED_IN" == "Y" ]]; then
+  echo "Okay, if you say so. Proceeding ..."
+else
+  echo "Please use 'docker login' before running this script"
+  exit 1
+fi
+
 echo "Building Application Image ..."
 docker build --no-cache -t ${REGISTRY}/${APP_REPO}:${TAG} -f Dockerfile .
-
-echo "Building Database Image ..."
-docker build --no-cache -t ${REGISTRY}/${DB_REPO}:${TAG} -f Dockerfile-db .
+if [ $? -ne 0 ]; then
+  echo "Application build failed!"
+  exit 1
+fi
 
 echo "Pushing Application Image to Registry ..."
 docker push ${REGISTRY}/${APP_REPO}:${TAG}
+if [ $? -ne 0 ]; then
+  echo "Failed to push Application Image to Registry!"
+  exit 1
+fi
 
-echo "Pushing Database Image to Registry ..."
-docker push ${REGISTRY}/${DB_REPO}:${TAG}
+echo "Successfully built and pushed image to registry!"
